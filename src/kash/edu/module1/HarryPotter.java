1256ed185ebbf874cb3ffe23b72b26c4fdf44281
Alona <elenakaschpruk@gmail.com>
2021-03-10 19:53:01 +0200

    10.03.21

---
+++ src/kash/edu/module1/HarryPotter.java
@@ -0,0 +1,14 @@
+package kash.edu.module1;
+/**
+  @author   Alona Kashpruk
+  @project   vsem
+  @class  HarryPotter
+  @version  1.0.0 
+  @since 10.03.2021 - 19.41
+**/
+
+public class HarryPotter {
+    public static void main(String[] args) {
+
+    }
+}
