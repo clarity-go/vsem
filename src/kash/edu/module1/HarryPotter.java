f5c64cd50165b49bd0b7458f33b061938c3eb196
Alona <elenakaschpruk@gmail.com>
2021-03-12 20:13:26 +0200

    12.03.21

--- src/kash/edu/module1/HarryPotter.java
+++ src/kash/edu/module1/HarryPotter.java
@@ -1,11 +1,11 @@
 package kash.edu.module1;
 
 /**
- @author   Alona Kashpruk
- @project   vsem
- @class  HarryPotter
- @version  1.0.0
- @since 10.03.2021 - 19.41
+  @author   Alona Kashpruk
+  @project   vsem
+  @class  HarryPotter
+  @version  1.0.0
+  @since 10.03.2021 - 19.41
  **/
 
 import java.io.IOException;
