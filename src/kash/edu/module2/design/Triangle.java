d3a0d48dd1642675a754b4400fc7f345cc11b71b
Alona <elenakaschpruk@gmail.com>
2021-03-17 20:04:53 +0200

    17.03.21

---
+++ src/kash/edu/module2/design/Triangle.java
@@ -0,0 +1,16 @@
+package kash.edu.module2.design;
+/**
+  @author   Alona Kashpruk
+  @project   vsem
+  @class  Triangle
+  @version  1.0.0 
+  @since 17.03.2021 - 20.02
+**/
+
+public class Triangle {
+    private double sideA;
+    private double sideB;
+
+    // 5 methods: one of them getArea, периметр, выста, медиана, угол...
+    //
+}
