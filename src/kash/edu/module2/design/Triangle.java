21b038052287c2ec02952fd21500c1ff1f21ae52
Alona <elenakaschpruk@gmail.com>
2021-03-17 20:11:53 +0200

    17.03.21

--- src/kash/edu/module2/design/Triangle.java
+++ src/kash/edu/module2/design/Triangle.java
@@ -13,4 +13,16 @@ public class Triangle {
 
     // 5 methods: one of them getArea, периметр, выста, медиана, угол...
     // 5 number: rhombus
+
+/*    Create a class from the attached document according to the order in your group .  The class must contain
+1. Constructor.
+2.. Getters/Setters.
+3. 5 methods.
+4. Override toString() method
+5. Override hash() and equals() methods.
+
+
+5. Rhombus.
+         */
+
 }
