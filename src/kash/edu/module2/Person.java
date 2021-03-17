f9232e9404a73e9d259eb365cf485722b941509f
Alona <elenakaschpruk@gmail.com>
2021-03-17 20:58:06 +0200

    17.03.21

---
+++ src/kash/edu/module2/Person.java
@@ -0,0 +1,18 @@
+package kash.edu.module2;
+
+import java.time.LocalDate;
+
+/**
+  @author   Alona Kashpruk
+  @project   vsem
+  @class  Person
+  @version  1.0.0 
+  @since 17.03.2021 - 20.56
+**/
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String patronymic;
+    private LocalDate dateOfBirth;
+}
