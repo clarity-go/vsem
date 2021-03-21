f4d4f43c3eaf83f1ef3c99adf4ba6f2e60a25ada
Alona <elenakaschpruk@gmail.com>
2021-03-21 13:50:22 +0200

    V2.1

--- src/kash/edu/module2/inheritance/Person.java
+++ src/kash/edu/module2/inheritance/Person.java
@@ -1,5 +1,7 @@
 package kash.edu.module2.inheritance;
 import java.time.LocalDate;
+import java.util.Objects;
+
 /**
   @author   Alona Kashpruk
   @project   vsem
@@ -7,11 +9,243 @@ import java.time.LocalDate;
   @version  1.0.0 
   @since 19.03.2021 - 18.37
 **/
-// no methods, but include getters and setters, (hashcode, string), 25 fields for 2 class, both
+
 public class Person {
+    private String gender;
+    private String race;
+    private String nationality;
     private String firstName;
     private String lastName;
     private String patronymicName;
+    private String alias;
     private LocalDate dateOfBirth;
-    private String Address;
+    private String country;
+    private String homeAddress;
+    private String email;
+    private boolean isMarried;
+    private boolean hasChildren;
+    private boolean isWorking;
+    private boolean hasProperty;
+    private boolean hasAuto;
+    private boolean hasDriversLicense;
+
+    public Person() {
+    }
+
+    public Person(String gender, String race, String nationality,
+                  String firstName, String lastName, String patronymicName,
+                  String alias, LocalDate dateOfBirth, String country,
+                  String homeAddress, String email, boolean isMarried,
+                  boolean hasChildren, boolean isWorking, boolean hasProperty,
+                  boolean hasAuto, boolean hasDriversLicense) {
+        this.gender = gender;
+        this.race = race;
+        this.nationality = nationality;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.patronymicName = patronymicName;
+        this.alias = alias;
+        this.dateOfBirth = dateOfBirth;
+        this.country = country;
+        this.homeAddress = homeAddress;
+        this.email = email;
+        this.isMarried = isMarried;
+        this.hasChildren = hasChildren;
+        this.isWorking = isWorking;
+        this.hasProperty = hasProperty;
+        this.hasAuto = hasAuto;
+        this.hasDriversLicense = hasDriversLicense;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPatronymicName() {
+        return patronymicName;
+    }
+
+    public void setPatronymicName(String patronymicName) {
+        this.patronymicName = patronymicName;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(String homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isMarried() {
+        return isMarried;
+    }
+
+    public void setMarried(boolean married) {
+        isMarried = married;
+    }
+
+    public boolean isHasChildren() {
+        return hasChildren;
+    }
+
+    public void setHasChildren(boolean hasChildren) {
+        this.hasChildren = hasChildren;
+    }
+
+    public boolean isWorking() {
+        return isWorking;
+    }
+
+    public void setWorking(boolean working) {
+        isWorking = working;
+    }
+
+    public boolean isHasProperty() {
+        return hasProperty;
+    }
+
+    public void setHasProperty(boolean hasProperty) {
+        this.hasProperty = hasProperty;
+    }
+
+    public boolean isHasAuto() {
+        return hasAuto;
+    }
+
+    public void setHasAuto(boolean hasAuto) {
+        this.hasAuto = hasAuto;
+    }
+
+    public boolean isHasDriversLicense() {
+        return hasDriversLicense;
+    }
+
+    public void setHasDriversLicense(boolean hasDriversLicense) {
+        this.hasDriversLicense = hasDriversLicense;
+    }
+
+    @Override
+    public String toString() {
+        return "Person {" +
+                "gender = '" + gender + '\'' +
+                ", race = '" + race + '\'' +
+                ", nationality = '" + nationality + '\'' +
+                ", firstName = '" + firstName + '\'' +
+                ", lastName = '" + lastName + '\'' +
+                ", patronymicName = '" + patronymicName + '\'' +
+                ", alias = '" + alias + '\'' +
+                ", dateOfBirth = " + dateOfBirth +
+                ", country = '" + country + '\'' +
+                ", homeAddress = '" + homeAddress + '\'' +
+                ", email = '" + email + '\'' +
+                ", isMarried = " + isMarried +
+                ", hasChildren = " + hasChildren +
+                ", isWorking = " + isWorking +
+                ", hasProperty = " + hasProperty +
+                ", hasAuto = " + hasAuto +
+                ", hasDriversLicense = " + hasDriversLicense +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return isMarried() == person.isMarried() && isHasChildren()
+                == person.isHasChildren() && isWorking()
+                == person.isWorking() && isHasProperty()
+                == person.isHasProperty() && isHasAuto()
+                == person.isHasAuto() && isHasDriversLicense()
+                == person.isHasDriversLicense()
+                && getGender().equals(person.getGender())
+                && getRace().equals(person.getRace())
+                && getNationality().equals(person.getNationality())
+                && getFirstName().equals(person.getFirstName())
+                && getLastName().equals(person.getLastName())
+                && getPatronymicName().equals(person.getPatronymicName())
+                && getAlias().equals(person.getAlias())
+                && getDateOfBirth().equals(person.getDateOfBirth())
+                && getCountry().equals(person.getCountry())
+                && getHomeAddress().equals(person.getHomeAddress())
+                && getEmail().equals(person.getEmail());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getGender(), getRace(), getNationality(),
+                getFirstName(), getLastName(), getPatronymicName(),
+                getAlias(), getDateOfBirth(), getCountry(), getHomeAddress(),
+                getEmail(), isMarried(), isHasChildren(), isWorking(),
+                isHasProperty(), isHasAuto(), isHasDriversLicense());
+    }
 }
--- src/kash/edu/module2/inheritance/Student.java
+++ src/kash/edu/module2/inheritance/Student.java
@@ -1,4 +1,8 @@
 package kash.edu.module2.inheritance;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
 /**
   @author   Alona Kashpruk
   @project   vsem
@@ -8,8 +12,151 @@ package kash.edu.module2.inheritance;
 **/
 
 public class Student extends Person{
+    private String university;
     private String group;
+    private String specialty;
     private String chair;
     private String faculty;
+    private String degreeType; // for example, Bachelor's, Master's degree
+    private LocalDate yearOfEntering; // year when entered the university
     private boolean isStipendiat; // has a scholarship
+
+    public Student() {
+    }
+
+    public Student(String university, String group, String specialty,
+                   String chair, String faculty, String degreeType,
+                   LocalDate yearOfEntering, boolean isStipendiat) {
+        this.university = university;
+        this.group = group;
+        this.specialty = specialty;
+        this.chair = chair;
+        this.faculty = faculty;
+        this.degreeType = degreeType;
+        this.yearOfEntering = yearOfEntering;
+        this.isStipendiat = isStipendiat;
+    }
+
+    public Student(String gender, String race, String nationality,
+                   String firstName, String lastName, String patronymicName,
+                   String alias, LocalDate dateOfBirth, String country,
+                   String homeAddress, String email, boolean isMarried,
+                   boolean hasChildren, boolean isWorking, boolean hasProperty,
+                   boolean hasAuto, boolean hasDriversLicense,
+                   String university, String group, String specialty,
+                   String chair, String faculty, String degreeType,
+                   LocalDate yearOfEntering, boolean isStipendiat) {
+        super(gender, race, nationality, firstName, lastName, patronymicName,
+                alias, dateOfBirth, country, homeAddress, email, isMarried,
+                hasChildren, isWorking, hasProperty, hasAuto, hasDriversLicense);
+        this.university = university;
+        this.group = group;
+        this.specialty = specialty;
+        this.chair = chair;
+        this.faculty = faculty;
+        this.degreeType = degreeType;
+        this.yearOfEntering = yearOfEntering;
+        this.isStipendiat = isStipendiat;
+    }
+
+    public String getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(String university) {
+        this.university = university;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+    public String getChair() {
+        return chair;
+    }
+
+    public void setChair(String chair) {
+        this.chair = chair;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public String getDegreeType() {
+        return degreeType;
+    }
+
+    public void setDegreeType(String degreeType) {
+        this.degreeType = degreeType;
+    }
+
+    public LocalDate getYearOfEntering() {
+        return yearOfEntering;
+    }
+
+    public void setYearOfEntering(LocalDate yearOfEntering) {
+        this.yearOfEntering = yearOfEntering;
+    }
+
+    public boolean isStipendiat() {
+        return isStipendiat;
+    }
+
+    public void setStipendiat(boolean stipendiat) {
+        isStipendiat = stipendiat;
+    }
+
+    @Override
+    public String toString() {
+        return " Student {" +
+                " university = '" + university + '\'' +
+                ", group = '" + group + '\'' +
+                ", specialty = '" + specialty + '\'' +
+                ", chair = '" + chair + '\'' +
+                ", faculty = '" + faculty + '\'' +
+                ", degreeType = '" + degreeType + '\'' +
+                ", yearOfEntering = " + yearOfEntering +
+                ", isStipendiat = " + isStipendiat +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Student student = (Student) o;
+        return isStipendiat() == student.isStipendiat()
+                && getUniversity().equals(student.getUniversity())
+                && getGroup().equals(student.getGroup())
+                && getSpecialty().equals(student.getSpecialty())
+                && getChair().equals(student.getChair())
+                && getFaculty().equals(student.getFaculty())
+                && getDegreeType().equals(student.getDegreeType())
+                && getYearOfEntering().equals(student.getYearOfEntering());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getUniversity(),
+                getGroup(), getSpecialty(), getChair(), getFaculty(),
+                getDegreeType(), getYearOfEntering(), isStipendiat());
+    }
 }
