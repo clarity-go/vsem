0ddf6b40df6f5ae804a8bb91abc4f75e00391a20
Alona <elenakaschpruk@gmail.com>
2021-03-31 20:43:42 +0300

    31.03.21

--- src/kash/edu/theory/module3try/LaboratoryWorkTry/Colls.java
+++ src/kash/edu/theory/module3try/LaboratoryWorkTry/Colls.java
@@ -14,9 +14,31 @@ public class Colls {
     public static void main(String[] args) {
         List<IAccounting> listOfCandies = new ArrayList<>(
                 Arrays.asList(
-                        new CandyBox("Swallow", 0.2, 100, 10, 2),
-                        new CandyWeight("Sparrow", 0.15, 50)
+                        new CandyBox("Swallow", 0.2, 150, 10, 2),
+                        new CandyBox("Betty", 0.5, 50, 10, 2),
+                        new CandyBox("Lilak", 0.2, 70, 10, 2),
+                        new CandyBox("Her&Him", 0.3, 80, 10, 2),
+                        new CandyBox("Time", 0.2, 90, 10, 2),
+                        new CandyWeight("Belissimo", 0.9, 70),
+                        new CandyWeight("Sparrow", 0.15, 65),
+                        new CandyWeight("Lake", 0.3, 55),
+                        new CandyWeight("Unknown", 0.15, 50),
+                        new CandyWeight("Sparrow", 0.15, 20)
                 )
         );
+        // calculate the revenue
+        double totalIncome = listOfCandies.stream()
+                .mapToDouble(IAccounting::getUltimatePrice).sum();
+
+        // get the most expensive buy
+        double expensive = listOfCandies.stream()
+                .mapToDouble(IAccounting::getUltimatePrice).max().getAsDouble();
+        System.out.println(expensive);
+
+        // get the most cheap buy
+        double cheap = listOfCandies.stream()
+                .mapToDouble(IAccounting::getUltimatePrice).min().getAsDouble();
+        System.out.println(cheap);
+
     }
 }
