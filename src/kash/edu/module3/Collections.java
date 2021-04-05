d273359177bfa48dd0c15af3f0a36255f875860d
Alona <elenakaschpruk@gmail.com>
2021-04-05 23:34:52 +0300

    Added the conclusions

--- src/kash/edu/module3/Collections.java
+++ src/kash/edu/module3/Collections.java
@@ -25,6 +25,7 @@ and on the end of ones. Compare time intervals.
 and from the end of ones. Compare time intervals.
 4. Delete 1000 elements  from the beginning, from the middle
 and from the end of ones. Compare time intervals.
+
 */
 
 public class Collections {
